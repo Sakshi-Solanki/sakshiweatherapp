681165360218edafb18a726e83d0147b337de6d9
Sakshi Solanki <sakshisolanki813@gmail.com>
2023-03-22 15:08:24 +0530

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6e0fe46fcamshb02ddcd42d692aep177b2bjsn45497ffea7a5',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+let weather = {
+	"apiKey": "e81e3b11971b9d95e2c3dce18be6ccf5",
+	fetchWeather: function (city) {
+		fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+		+ city 
+		+ "&units=metric&appid=" 
+		+ this.apiKey
+		)
+		.then((response) => response.json())
+		.then((data) => this.displayWeather(data));
+	},
+	displayWeather: function(data){
+        const { name } = data;
+		const { icon, description } = data.weather[0];
+		const{ temp, humidity } = data.main;
+		const { speed } = data.wind;
+		console.log(name,icon,description,temp,humidity,speed)
+
+		document.querySelector(".city").innerText = "Weather In " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon +".png";
+		document.querySelector(".description").innerHTML = description;
+		document.querySelector(".temp").innerHTML = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+		document.querySelector(".wind").innerText = "Wind Speed:  " + speed + "km/hr";
+		document.querySelector(".weather").classList.remove("loading");
+	},
+	search: function () {
+		this.fetchWeather(document.querySelector(".search-bar").value);
 	}
 };
 
-const getWeather = (city)=>{
+document.querySelector(".search button").addEventListener("click",function() {
+  weather.search();
+});
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle', options)
-	.then(response => response.json())
-	.then(response => { 
-		console.log(response)
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+	if(event.key == "Enter"){
+		weather.search();
+	}
+});
 
-		
-	 }) 
-	.catch(err => console.error(err));
-}
+weather.fetchWeather("Modasa");
